d448bb66068a12583471d5d3237bd134ed0592a3
narimami <naritsukamamiko@outlook.jp>
2020-10-09 19:45:57 +0900

    second-commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,52 +1,72 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
-import HeaderAuth from '../components/HeaderAuth.vue'
-import SignUp from '../views/SignUp.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import SignUp from "../views/SignUp.vue";
 import Profile from "../views/Profile.vue";
 import Detail from "../views/Detail.vue";
+import store from "../store/index";
 
+Vue.use(VueRouter);
 
-Vue.use(VueRouter)
-
-const routes = [{
- path: '/',
- name: 'Login',
- component: Login
-}, 
-{
- path: '/home',
- name: 'Home',
- component: Home
-},
-{
-  path: '/headerAuth',
- name: 'HeaderAuth',
- component: HeaderAuth
-},
-{
-  path: '/signUp',
- name: 'SignUp',
- component: SignUp
-},
-{
-  path: "/detail/:id",
-  name: "detail",
-  component: Detail,
-  props: true,
-},
-{
-  path: "/profile",
-  name: "profile",
-  component: Profile,
-},
-]
+const routes = [
+  {
+    path: "/",
+    name: "login",
+    component: Login,
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: SignUp,
+  },
+  {
+    path: "/home",
+    name: "Home",
+    component: Home,
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/detail/:id",
+    name: "detail",
+    component: Detail,
+    meta: {
+      requiresAuth: true,
+    },
+    props: true,
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    component: Profile,
+    meta: {
+      requiresAuth: true,
+    },
+  },
+];
 
 const router = new VueRouter({
- mode: 'history',
- base: process.env.BASE_URL,
- routes
-})
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  if (
+    to.matched.some((record) => record.meta.requiresAuth) &&
+    !store.state.auth
+  ) {
+    next({
+      path: "/",
+      query: {
+        redirect: to.fullPath,
+      },
+    });
+  } else {
+    next();
+  }
+});
+
+export default router;
